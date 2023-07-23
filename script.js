d962404a1bb07e3ff6036fef7a2543f8e78d41eb
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-07-23 16:59:33 +0100

    added modal via alpine js

---
+++ script.js
@@ -0,0 +1,162 @@
+'use strict';
+
+if (document.querySelector('.splash')) {
+  document.addEventListener('click', function () {
+    window.location.href = './spalsh2.html';
+  });
+}
+if (document.querySelector('.splash2')) {
+  document.querySelector('.english').addEventListener('click', () => {
+    window.location.href = './onboard.html';
+  });
+}
+if (document.querySelector('.phoneOTP')) {
+  document.querySelector('.submit').addEventListener('click', () => {
+    document.querySelector('.blur-section').classList.add('blur');
+    document.querySelector('.checked').classList.remove('hide');
+  });
+
+  document.querySelector('.check-con').addEventListener('click', () => {
+    window.location.href = './home.html';
+  });
+}
+
+if (document.querySelector('.login')) {
+  document.querySelector('.submit').addEventListener('click', () => {
+    document.querySelector('.blur-section').classList.add('blur');
+    document.querySelector('.checked').classList.remove('hide');
+  });
+
+  document.querySelector('.cancel-bio').addEventListener('click', () => {
+    document.querySelector('.blur-section').classList.remove('blur');
+    document.querySelector('.checked').classList.add('hide');
+  });
+}
+
+if (document.querySelector('.airtime')) {
+  console.log('I got here');
+  const see = document.querySelector('.next');
+  console.log(see);
+  see.addEventListener('click', function () {
+    console.log('I am clicked');
+  });
+
+  const hedd = document.querySelector('.hedd');
+  console.log(hedd);
+  if (hedd) {
+    hedd.addEventListener('click', function () {
+      console.log('I am clicked');
+    });
+  }
+}
+
+if (document.querySelector('.swap-convert')) {
+  document.querySelector('.swap-prev').addEventListener('click', () => {
+    document.querySelector('.blur-section').classList.add('blur');
+    document.querySelector('.swap-modal').classList.remove('hidden');
+  });
+
+  document.querySelector('.swap-cancel').addEventListener('click', () => {
+    document.querySelector('.blur-section').classList.remove('blur');
+    document.querySelector('.swap-modal').classList.add('hidden');
+  });
+
+  document.querySelector('.swap-submit').addEventListener('click', () => {
+    window.location.href = './tracasction-success.html?source=convert';
+  });
+}
+
+if (document.querySelector('.transaction-sucessfull')) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const source = urlParams.get('source');
+
+  if (source === 'send') {
+    document.querySelector('.note-head').textContent = 'Transaction successful';
+    let paragraphs = document.getElementsByTagName('p');
+    paragraphs[0].textContent = 'Transfered NGN 100,000';
+    paragraphs[1].textContent = 'To Reece James';
+
+    document.querySelector('.trac-done').addEventListener('click', () => {
+      window.location.href = './home.html';
+    });
+    document.querySelector('.view-receipt').addEventListener('click', () => {
+      window.location.href = './transaction-receipt.html';
+    });
+  }
+
+  if (source === 'convert') {
+    document.querySelector('.note-head').textContent = 'Swap successful';
+
+    let paragraphs = document.getElementsByTagName('p');
+    paragraphs[0].textContent = '';
+    paragraphs[1].textContent =
+      'You have successfully swapped 100 USD to 74,000 NGN ';
+
+    document.querySelector('.trac-done').addEventListener('click', () => {
+      console.log('go back to home');
+      window.location.href = './home.html';
+    });
+    document.querySelector('.view-receipt').addEventListener('click', () => {
+      console.log('show receipt');
+    });
+  }
+}
+
+if (document.querySelector('.send-amount')) {
+  document.querySelector('.send-prev').addEventListener('click', () => {
+    document.querySelector('.send-amount-body').classList.add('blur');
+    document.querySelector('.send-modal').classList.remove('hidden');
+  });
+
+  document.querySelector('.cancel-prev').addEventListener('click', () => {
+    document.querySelector('.send-amount-body').classList.remove('blur');
+    document.querySelector('.send-modal').classList.add('hidden');
+  });
+
+  document.querySelector('.send-confirm').addEventListener('click', () => {
+    window.location.href = './tracasction-success.html?source=send';
+  });
+}
+
+if (document.querySelector('.home')) {
+  const topUp = document.querySelector('.top-up');
+  const topUpModal = document.querySelector('.top-up-modal');
+  const closeModal = document.querySelector('.close-modal');
+  const overlay = document.querySelector('.overlay');
+
+  topUp.addEventListener('click', () => {
+    topUpModal.classList.remove('hide');
+    overlay.classList.remove('hide');
+  });
+  closeModal.addEventListener('click', () => {
+    topUpModal.classList.add('hide');
+    overlay.classList.add('hide');
+  });
+
+  const send = document.querySelector('.send');
+  const sendModal = document.querySelector('.send-modal');
+  const closeSendModal = document.querySelector('.close-send-modal');
+
+  send.addEventListener('click', () => {
+    sendModal.classList.remove('hide');
+    overlay.classList.remove('hide');
+  });
+
+  closeSendModal.addEventListener('click', () => {
+    sendModal.classList.add('hide');
+    overlay.classList.add('hide');
+  });
+
+  const utility = document.querySelector('.utility');
+  const utilityModal = document.querySelector('.utility-modal');
+  const closeUtilityModal = document.querySelector('.close-utility-modal');
+
+  utility.addEventListener('click', () => {
+    utilityModal.classList.remove('hide');
+    overlay.classList.remove('hide');
+  });
+  closeUtilityModal.addEventListener('click', () => {
+    utilityModal.classList.add('hide');
+    overlay.classList.add('hide');
+  });
+}
